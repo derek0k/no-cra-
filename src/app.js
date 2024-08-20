eb1c343e2fabb615b18f73d8aefc6205964cff00
derek0k <derek0kim@gmail.com>
2024-08-20 19:50:25 +0900

    모듈 함수 추가 및 App 컴포넌트 import 업데이트

--- src/app.js
+++ src/app.js
@@ -1,5 +1,16 @@
+import add from "./add.js";
+import sub from "./sub.js";
+import mul from "./mul.js";
+
 const App = () => {
-  return <h1>Hello, React!</h1>;
+  const sum = add(2, 3);
+  const diff = sub(2, 3);
+  const product = mul(2, 3);
+  return (
+    <h1>
+      {sum}, {diff}, {product}
+    </h1>
+  );
 };
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
